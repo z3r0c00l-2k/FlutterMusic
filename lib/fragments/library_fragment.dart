1a9c8e4e17699356723754b56979fae13f416768
z3r0c00l-2k <mjack1327@gmail.com>
2019-11-18 19:04:05 +0530

    adding future builder

--- lib/fragments/library_fragment.dart
+++ lib/fragments/library_fragment.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_music/themes/colors.dart';
 
 class LibraryFragment extends StatefulWidget {
   @override
@@ -7,19 +11,9 @@ class LibraryFragment extends StatefulWidget {
 }
 
 class _LibraryFragmentState extends State<LibraryFragment> {
-  List<Song> _songs = List();
-
-  @override
-  void initState() {
-    super.initState();
-    initMusicList();
-  }
-
-  void initMusicList() async {
+  Future<List<Song>> _getMusicList() async {
     List<Song> songs = await MusicFinder.allSongs();
-    setState(() {
-      _songs = songs;
-    });
+    return songs;
   }
 
   @override
@@ -40,36 +34,105 @@ class _LibraryFragmentState extends State<LibraryFragment> {
         ),
         SizedBox(
           height: MediaQuery.of(context).size.height - 180,
-          child: ListView.builder(
-              itemCount: _songs.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            _songs[index].title,
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            _songs[index].artist,
-                            style: TextStyle(fontSize: 12.0),
-                          ),
-                          Text(
-                            _songs[index].duration.toString(),
-                            style: TextStyle(fontSize: 12.0),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }),
+          child: FutureBuilder(
+            future: _getMusicList(),
+            builder:
+                // ignore: missing_return
+                (BuildContext context, AsyncSnapshot<List<Song>> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Text('Press button to start.');
+                case ConnectionState.active:
+                case ConnectionState.waiting:
+                  return Text('Loading..');
+                case ConnectionState.done:
+                  if (snapshot.hasError)
+                    return Text('Error: ${snapshot.error}');
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          Widget albumArt;
+                          try {
+                            albumArt = CircleAvatar(
+                                radius: 30.0,
+                                backgroundImage: FileImage(
+                                    File(snapshot.data[index].albumArt)));
+                          } catch (e) {
+                            albumArt = CircleAvatar(
+                                radius: 30.0,
+                                child: Text(snapshot.data[index].title[0]
+                                    .toUpperCase()));
+                          }
+
+                          return Row(
+                            children: <Widget>[
+                              Flexible(
+                                child: Container(
+                                  margin: EdgeInsets.only(bottom: 10.0),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 15.0),
+                                        child: albumArt,
+                                      ),
+                                      Flexible(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(
+                                              snapshot.data[index].title,
+                                              softWrap: false,
+                                              overflow: TextOverflow.fade,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            Text(
+                                              snapshot.data[index].artist,
+                                              style: TextStyle(
+                                                  fontSize: 12.0,
+                                                  color:
+                                                      AppColors.lightTextColor),
+                                            ),
+                                            Text(
+                                              _formatDuration(snapshot
+                                                  .data[index].duration),
+                                              style: TextStyle(fontSize: 12.0),
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }),
+                  );
+              }
+            },
+          ),
         )
       ],
     );
+  }
+
+  String _formatDuration(int dur) {
+    Duration duration = Duration(milliseconds: dur);
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    if (duration.inHours < 0)
+      return "${duration.inHours}:$twoDigitMinutes:$twoDigitSeconds";
+    else
+      return "$twoDigitMinutes:$twoDigitSeconds";
   }
 }
--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static Color iconColor = Color(0xffFC5830);
   static Color iconColorUnselected = Color(0xffffae9a);
   static Color fabColor = Color(0xfff9bb2d);
+  static Color lightTextColor = Color(0xff9A9A9A);
 }
